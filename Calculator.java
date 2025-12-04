3a93292b8e5b0b747f4040e4b88eb11314014c08
Aftab043 <aftabansari78210@gmail.com>
2025-12-04 13:41:52 +0530

    Fix calculation and output formatting in Calculator

--- Calculator.java
+++ Calculator.java
@@ -1,14 +1,4 @@
-package Problem_2;
-
-import java.util.Scanner;
-
-// Make a calculator.Take 2 numbers (a & b) from the user and an operation as follows 
-// 1 --> + (Addition) a + b  
-// 2 --> - (Substraction) a - b 
-// 3 --> * (Multiplication) a * b 
-// 4 --> / (Division) a / b 
-// 5 --> % (Moodulo or Remainder) a % b 
-// Calculate the result according to the operation given and display it to the user 
+import java.util.Scanner; 
 
 public class Question_1 {
     public static void main(String[] args) {
@@ -27,24 +17,25 @@ public class Question_1 {
 
         switch (operand) {
             case '+':
-                result = (a + b);
-                System.out.println(+ a + " + " + b + " = " + (+result));
+                result = (float)a + b;
+                System.out.println(+ a + " + " + b + " = " + result);
                 break;
             case '-':
                 result = (a - b);
-                System.out.println(+ a + " - " + b + " = " + (+result));
+                System.out.println(+ a + " - " + b + " = " + result);
                 break;
             case '*':
                 result = (a * b);
-                System.out.println(+ a + " * " + b + " = " + (+result));
+                System.out.println(+ a + " * " + b + " = " + result);
                 break;
             case '/':
-                result = (float)(a / b);
-                System.out.println(+ a + " / " + b + " = " + (+result));
+                result = (float)a / b;
+                System.out.println(+ a + " / " + b + " = " + result);
                 break;
             case '%':
                 result = (a % b);
-                System.out.println(+ a + " % " + b + " = " + (+result));
+                System.out.println(+ a + " % " + b + " = " + result);
+                break;
             default:
                 System.out.println("Invalid operand please keep try");
                 break;
@@ -52,3 +43,4 @@ public class Question_1 {
 
     }
 }
+
